bd6d64a9d4e738a18432bf8edb338145a302cec4
Miguel Julián <mjulian@zentyal.com>
2014-04-24 09:34:51 +0200

    First working search word script

--- search-word.py
+++ search-word.py
@@ -17,8 +17,46 @@ def parse_parameters(parameters):
 
     return (parameters[1], parameters[2:])
 
+def look_word_in_worksheet(word, workbook, worksheet_name):
+    worksheet = workbook.sheet_by_name(worksheet_name)
+
+    results = {}
+    for current_row in range(worksheet.nrows):
+        row = []
+
+        for current_col in range(worksheet.ncols):
+            cell_value = worksheet.cell_value(current_row, current_col)
+            #FIXME Should we check cell_type?
+            if cell_value == word:
+                row.append(current_col)
+
+        if row:
+            results[current_row] = row
+
+    return results
+
 def look_word_in_file(word, filename):
-    return [filename]
+    workbook = xlrd.open_workbook(filename)
+    results = {}
+    for worksheet_name in workbook.sheet_names():
+        worksheet_result = look_word_in_worksheet(word, workbook, worksheet_name)
+        if worksheet_result:
+            results[worksheet_name] = worksheet_result
+
+    return results
+
+def show_results(results):
+    message = ""
+
+    for workbook_name, workbook_results in results.items():
+        message += "Results found at " + workbook_name + ":\n"
+        for worksheet_name, worksheet_results in workbook_results.items():
+            message += "\tResults found at " + worksheet_name + " worksheet:\n"
+            for row, col in worksheet_results.items():
+                message += "\t\tResult found at row " + str(row + 1) + " (at cols " + str(col) + ")\n"
+
+    return message
+
 
 # Here starts the program
 word, files = parse_parameters(sys.argv)
@@ -30,4 +68,4 @@ results = {}
 for workbook_name in files:
     results[workbook_name] = look_word_in_file(word, workbook_name)
 
-print "The word has been found here:\n" + str(results)
+print "\n" + show_results(results)
